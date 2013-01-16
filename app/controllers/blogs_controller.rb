2ea91e172abcf200cc79611e4180cd5bf10f0a32
Jin <jin.xie@alliants.co.uk>
2013-01-16 17:37:20 +0000

    Implement blog CRUD
    -basic tests controllers view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,38 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params[:blog])
+    if @blog.save
+      redirect_to blogs_path
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update_attributes(params[:blog])
+      redirect_to @blog
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    if @blog.destroy
+      redirect_to blogs_path
+    end
+  end
+
 end
--- spec/acceptance/blogs_spec.rb
+++ spec/acceptance/blogs_spec.rb
@@ -11,14 +11,91 @@ feature "blogs page", %q{
     click_on 'Blogs'
   end
 
-  scenario "create a new blog" do
-    page.should have_link("New Blog")
+  context "create a new blog" do
+    let(:new_blog_content) { "new example content" }
+
+    before(:each) do
+      page.should have_link("New Blog")
+      click_on "New Blog"
+    end
+
+    scenario "launching blog new page and fill in content" do
+      current_path.should == new_blog_path
+      fill_in 'blog[content]', :with => new_blog_content
+      click_on "Post"
+      page.should have_content(new_blog_content)
+    end
   end
 
-  scenario "view all blogs" do
-    blog_content = "example blog content"
-    Blog.create(content: blog_content)
-    visit current_path
-    page.should have_content(blog_content)
+  context "view list of blogs" do
+    let(:content_1) { "alpha example blog content" }
+    let(:content_2) { "beta example blog content" }
+
+    before(:each) do
+      Blog.create(content: content_1)
+      Blog.create(content: content_2)
+      visit current_path
+    end
+
+    scenario "launching the blogs index page" do
+      blog_content = "example blog content"
+      Blog.create(content: blog_content)
+      visit current_path
+      page.should have_content(blog_content)
+    end
+  end
+
+  context "inspect a blog" do
+    let(:blog_content) { "alpha example blog content" }
+    let(:blog) { Blog.create(content: blog_content) }
+
+    before(:each) do
+      blog
+      visit current_path
+      click_on blog_content
+    end
+
+    scenario "launching blog show page" do
+      current_path.should == blog_path(blog)
+      page.should have_content(blog_content)
+    end
+  end
+
+  context "edit a blog" do
+    let(:blog_content) { "alpha example blog content" }
+    let(:new_blog_content) { "beta example blog content" }
+    let(:blog) { Blog.create(content: blog_content) }
+
+    before(:each) do
+      blog
+      visit current_path
+      click_on blog_content
+      click_on "Edit"
+    end
+
+    scenario "launching edit blog page and edit content" do
+      current_path.should == edit_blog_path(blog)
+      fill_in 'blog[content]', :with => new_blog_content
+      click_on "Post"
+      page.should have_no_content(blog_content)
+      page.should have_content(new_blog_content)
+    end
+  end
+
+  context "remove a blog" do
+    let(:blog_content) { "alpha example blog content" }
+    let(:blog) { Blog.create(content: blog_content) }
+
+    before(:each) do
+      blog
+      visit current_path
+      click_on blog_content
+      click_on "Remove"
+    end
+
+    scenario "after blog is removed" do
+      current_path.should == blogs_path
+      page.should have_no_content(blog_content)
+    end
   end
 end
